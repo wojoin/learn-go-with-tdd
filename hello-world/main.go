ddd79b026817ad790efec3856d5e2989c4f16cc3
jianyin.wu <jianyin.wu@horizon.ai>
2019-10-14 17:22:34 +0800

    加入多个语言的支持

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,15 +3,28 @@ package main
 import "fmt"
 
 const helloPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello print greet
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
+	}
+
 	return helloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello-world/main_test.go
+++ hello-world/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import "testing"
 
-// 需求是当我们的函数用空字符串调用时，它默认为打印 "Hello, World" 而不是 "Hello, "
 func TestHello(t *testing.T) {
 
 	assertCorrectMessage := func(t *testing.T, got, want string) {
@@ -13,14 +12,29 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
+	// 现在需要用空字符串调用时，它默认为打印 "Hello, World" 而不是 "Hello, "
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// 现在需要支持第二个参数，指定问候的语言
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// French
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", french)
+		want := "Bonjour, Lauren"
 		assertCorrectMessage(t, got, want)
 	})
 
