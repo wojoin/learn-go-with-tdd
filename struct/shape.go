d7aa961fdf3b426f0ea07aa48b27e47bed5cebc3
join <wojoin98@gmail.com>
2019-10-14 23:41:53 +0800

    使用Rectangle结构体作为参数来表达Perimeter, Area计算的是长方形的周长和面积

--- struct/shape.go
+++ struct/shape.go
@@ -2,14 +2,19 @@ package main
 
 import "fmt"
 
-func Perimeter(width float64, height float64) float64 {
-    return 2*(width + height)
+type Rectangle struct {
+	Width float64
+	Height float64
 }
 
-func Area(width float64, height float64) float64 {
-    return width * height
+func Perimeter(r Rectangle) float64 {
+    return 2*(r.Width + r.Height)
+}
+
+func Area(r Rectangle) float64 {
+    return r.Width * r.Height
 }
 
 func main() {
-	fmt.Println(Perimeter(10.0, 20.0))
+	fmt.Println(Perimeter(Rectangle{10.0, 20.0}))
 }
--- struct/shape_test.go
+++ struct/shape_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	r := Rectangle{10.0, 10.0}
+	got := Perimeter(r)
 	want := 40.0
 
 	if got != want {
@@ -12,7 +13,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	r := Rectangle{12.0, 6.0}
+	got := Area(r)
 	want := 72.0
 
 	if got != want {
